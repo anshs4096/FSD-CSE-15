c19f3d0ea01075339b101e377d7fd5332a823f23
Anshuman Singh <anshumansingh891700@gmail.com>
2026-09-23 14:48:12 +0530

    hello

--- Shopping-App/client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="footer">
-<h3>Design and Devloped By Ashish Bajpai &copy;2026</h3>      
-    </div>
-  )
-}
-
-export default Footer
--- ex2.js
+++
@@ -1,7 +0,0 @@
-// function sum(a,b){
-//     return (a+b);
-// }
-
-const sum=(a=0,b=0)=>a+b;
-
-console.log("sum=",sum())
